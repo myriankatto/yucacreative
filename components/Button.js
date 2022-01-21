11e00911faa8debdfa4e4cd72efb6641b3f65b42
Myrian <myrian.katto@xapo.com>
2022-01-21 16:42:37 +0100

    Revert "first commit"
    This reverts commit 7041d287c58e3d073f4d62df32dff0566c5b0682.

--- components/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ButtonStyles from "../styles/Button.module.scss";
-
-const Button = ({ title, github }) => {
-  return <div className={ButtonStyles.button}>{title}</div>;
-};
-
-export default Button;
